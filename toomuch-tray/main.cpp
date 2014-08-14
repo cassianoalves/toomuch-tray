e27de38c06f58507c872e356c1a536b11cc501f7
Cassiano Alves <cassiano.alves@gmail.com>
2014-08-14 01:22:02 -0300

    Removendo antigos e testes

--- toomuch-tray/main.cpp
+++ toomuch-tray/main.cpp
@@ -1,11 +1,21 @@
 #include "mainwindow.h"
 #include <QApplication>
+#include <QSystemTrayIcon>
+#include <QPixmap>
 
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
-    MainWindow w;
-    w.show();
+
+    QSystemTrayIcon *tray = new QSystemTrayIcon();
+    QPixmap content;
+    content.fill(Qt::red);
+    QIcon ico(content);
+    tray->setIcon(ico);
+    tray->show();
+
+    //MainWindow w;
+    //w.show();
 
     return a.exec();
 }
